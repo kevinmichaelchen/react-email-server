2fb475266ef4043d21ea39e10b7dbaffe6661c4e
Kevin Chen <kevinmichaelchen@gmail.com>
2023-06-22 11:41:46 -0400

    feat: Integrate react-email (#2)

--- connect.tsx
+++ connect.tsx
@@ -1,13 +1,21 @@
+import * as React from 'react';
 import { ConnectRouter } from "@bufbuild/connect";
 import { EmailService } from "./gen/api_connect";
+
+import NotionMagicLinkEmail from './emails/notion-magic-link';
+import { render } from '@react-email/render';
 
 export default (router: ConnectRouter) =>
   // registers buf.connect.demo.eliza.v1.EmailService
   router.service(EmailService, {
     // implements rpc CreateWelcomeEmail
     async createWelcomeEmail(req) {
+      const name = req.name;
+      const html = render(<NotionMagicLinkEmail />, {
+        pretty: true,
+      });
       return {
-        html: `You said: ${req.name}`
+        html: html
       }
     },
   });
